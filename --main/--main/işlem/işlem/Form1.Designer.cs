316bbf45f3ac2fda5d3b529c5275a165142010cd
simademirbag <150009373+simademirbag@users.noreply.github.com>
2023-11-06 11:58:29 +0300

    Add files via upload

---
+++ --main/--main/işlem/işlem/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace işlem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCevap = new System.Windows.Forms.TextBox();
+            this.lblBaşla = new System.Windows.Forms.Label();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.btnCevap = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(114, 12);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(64, 31);
+            this.txtSayi2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "=";
+            // 
+            // txtCevap
+            // 
+            this.txtCevap.Location = new System.Drawing.Point(214, 12);
+            this.txtCevap.Name = "txtCevap";
+            this.txtCevap.Size = new System.Drawing.Size(64, 31);
+            this.txtCevap.TabIndex = 4;
+            // 
+            // lblBaşla
+            // 
+            this.lblBaşla.AutoSize = true;
+            this.lblBaşla.Location = new System.Drawing.Point(43, 110);
+            this.lblBaşla.Name = "lblBaşla";
+            this.lblBaşla.Size = new System.Drawing.Size(179, 25);
+            this.lblBaşla.TabIndex = 5;
+            this.lblBaşla.Text = "OYUNA BAŞLA...";
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(12, 58);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(266, 35);
+            this.btnYeni.TabIndex = 6;
+            this.btnYeni.Text = "YENİ SORU ";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // btnCevap
+            // 
+            this.btnCevap.Location = new System.Drawing.Point(284, 12);
+            this.btnCevap.Name = "btnCevap";
+            this.btnCevap.Size = new System.Drawing.Size(162, 81);
+            this.btnCevap.TabIndex = 7;
+            this.btnCevap.Text = "CEVAP";
+            this.btnCevap.UseVisualStyleBackColor = true;
+            this.btnCevap.Click += new System.EventHandler(this.btnCevap_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(452, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 154);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "YANLIŞ:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "DOĞRU:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 191);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCevap);
+            this.Controls.Add(this.btnYeni);
+            this.Controls.Add(this.lblBaşla);
+            this.Controls.Add(this.txtCevap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCevap;
+        private System.Windows.Forms.Label lblBaşla;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.Button btnCevap;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ --main/--main/işlem/işlem/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace işlem
+{
+    public partial class Form1 : Form
+       
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi1, sayi2;
+        int dogruSayisi = 0, yanlisSayisi = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {     
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            sayi1 = rnd.Next(0, 101);
+            sayi2 = rnd.Next(0, 101);
+            textBox1.Text = sayi1.ToString();
+            txtSayi2.Text = sayi2.ToString();
+            btnCevap.Enabled = true;
+            btnYeni.Enabled = false;
+        }
+
+        private void btnCevap_Click(object sender, EventArgs e)
+        {
+            int verilenCevap = Convert.ToInt32(txtCevap.Text);
+            int cevap = sayi1 * sayi2;
+            if (verilenCevap == cevap)
+            {
+                lblBaşla.Text = "Tebrikler...";
+                dogruSayisi++;
+                SoundPlayer player = new SoundPlayer();
+                string path = "C:\\Users\\90544\\Downloads\\alkış.wav";
+                player.SoundLocation = path;
+                player.Play();  
+            }
+            else
+            {
+                lblBaşla.Text = "Cevabınız Hatalı!!!";
+                yanlisSayisi++;
+                listBox1.Items.Add(sayi1 + "*" + sayi2 + "=" + verilenCevap);
+                MessageBox.Show("Doğru Cevap: " + cevap + " olmalı.");
+            }
+            textBox1.Text = "";
+            txtSayi2.Text = "";
+            txtCevap.Text = "";
+            btnCevap.Enabled = false;
+            btnYeni.Enabled = true;
+            label5.Text = "Doğru: " + dogruSayisi;
+            label4.Text = "Yanlış: " + yanlisSayisi;
+        }
+    }
+}
